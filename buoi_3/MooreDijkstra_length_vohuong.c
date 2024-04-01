f5e948f255e8d7b51aad9f23bb8f49d2b876bd8f
phucthienk48 <165442218+phucthienk48@users.noreply.github.com>
2024-04-01 21:28:01 +0700

    update

---
+++ buoi_3/MooreDijkstra_length_vohuong.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX_N 100
+
+typedef struct Edge{
+	int u, v;
+	int weight;
+}Edge;
+
+typedef struct Graph{
+	int n, m;
+	Edge edges[MAX_N];
+}Graph;
+
+void init_graph(Graph *pG, int n){
+	pG->n = n;
+	pG->m = 0;
+}
+
+void add_edges(Graph *pG, int u, int v, int w){
+	pG->edges[pG->m].u = u;
+	pG->edges[pG->m].v = v;
+	pG->edges[pG->m].weight = w;
+	
+	pG->m++;
+}
+
+int adjacent(Graph *pG, int u, int v){
+	int i;
+	for(i=0;i<pG->m;++i){
+		if((pG->edges[i].u == u && pG->edges[i].v == v) ||(pG->edges[i].u == v && pG->edges[i].v == u)){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int pi[MAX_N];
+int p[MAX_N];
+int mark[MAX_N];
+
+#define oo INT_MAX
+
+int getWeight(Graph *pG, int u, int v){
+	int i;
+	for(i=0;i<pG->m;++i){
+		if((pG->edges[i].u == u && pG->edges[i].v == v) ||(pG->edges[i].u == v && pG->edges[i].v == u)){
+			return pG->edges[i].weight;
+		}
+	}
+	return 0;
+}
+
+void MooreDijkstra(Graph *pG, int s){
+	int i, j, v;
+	for(i=1;i<=pG->n;++i){
+		pi[i] = oo;
+		p[i] = 0;
+		mark[i] = 0;
+	}
+	
+	pi[s] = 0;
+	p[s] = -1;
+	
+	for(i=1;i<=pG->n-1;++i){
+		
+		int min_pi = oo;
+		int min_index;
+		for(j=1;j<=pG->n;++j){
+			if(pi[j]<min_pi && !mark[j]){
+				min_index = j;
+				min_pi = pi[j];
+			}
+		}
+	
+		mark[min_index] = 1;
+		
+		for(v=1;v<=pG->n;++v){
+			if(adjacent(pG, min_index, v) && !mark[v]){
+				int w = getWeight(pG, min_index, v);
+				if(pi[min_index] + w < pi[v]){
+					pi[v] = pi[min_index] + w;
+					p[v] = min_index;
+				}
+			}
+		}
+		
+	}
+}
+
+int main(void){
+	freopen("dt.txt", "r", stdin);
+	
+	Graph G;
+	
+	int n,m;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	int i;
+	for(i = 0;i<m;++i){
+		int u, v, w;
+		scanf("%d%d%d", &u, &v, &w);
+		add_edges(&G, u,v,w);
+	}
+	
+	MooreDijkstra(&G, 1);
+	
+	printf("%d", (pi[n] == oo) ? -1 : pi[n]);
+	
+	return 0;
+}
